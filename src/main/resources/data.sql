e366929a4638cc231a25350c6b3ac7fb3cdec717
ion <ion@highwide.jp>
2025-08-09 06:56:14 +0900

    DBをファイルに

--- src/main/resources/data.sql
+++ src/main/resources/data.sql
@@ -2,10 +2,10 @@ insert into channel_master (channel_id, channel_name) values ('12867532669858611
 insert into channel_master (channel_id, channel_name) values ('1286759459242381433','アクレコ雑談');
 insert into channel_master (channel_id, channel_name) values ('1286758991875276880','傭兵団戦会議室');
 
-insert into alliance_member (ayarabu_id, ayarabu_name) values ('mitsu','YN');
-insert into alliance_member (ayarabu_id, ayarabu_name) values ('mitsu','レイ');
-insert into alliance_member (ayarabu_id, ayarabu_name) values ('mitsu','lum叁lum叁');
-insert into alliance_member (ayarabu_id, ayarabu_name) values ('mitsu','JonMiller');
-insert into alliance_member (ayarabu_id, ayarabu_name) values ('mitsu','ゆきち');
-insert into alliance_member (ayarabu_id, ayarabu_name) values ('mitsu','アキト');
-insert into alliance_member (ayarabu_id, ayarabu_name) values ('mitsu','サオ');
+insert into alliance_member (ayarabu_id, ayarabu_name, member_role, bot, alliance) values ('mitsu','YN', 'MEMBER', 0, 'NONE');
+insert into alliance_member (ayarabu_id, ayarabu_name, member_role, bot, alliance) values ('mitsu','レイ', 'MEMBER', 0, 'NONE');
+insert into alliance_member (ayarabu_id, ayarabu_name, member_role, bot, alliance) values ('mitsu','lum叁lum叁', 'MEMBER', 0, 'NONE');
+insert into alliance_member (ayarabu_id, ayarabu_name, member_role, bot, alliance) values ('mitsu','JonMiller', 'MEMBER', 0, 'NONE');
+insert into alliance_member (ayarabu_id, ayarabu_name, member_role, bot, alliance) values ('mitsu','ゆきち', 'MEMBER', 0, 'NONE');
+insert into alliance_member (ayarabu_id, ayarabu_name, member_role, bot, alliance) values ('mitsu','アキト', 'MEMBER', 0, 'NONE');
+insert into alliance_member (ayarabu_id, ayarabu_name, member_role, bot, alliance) values ('mitsu','サオ', 'MEMBER', 0, 'NONE');
